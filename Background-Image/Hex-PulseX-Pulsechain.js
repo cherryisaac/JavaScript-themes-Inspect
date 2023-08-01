4458279d0a34c9cc89ed3e4d5f3e6e13a4c6371b
Isaac <97069528+cherryisaac@users.noreply.github.com>
2023-08-01 18:14:09 -0400

    Create Hex-PulseX-Pulsechain.js

---
+++ Background-Image/Hex-PulseX-Pulsechain.js
@@ -0,0 +1,28 @@
+var e = document.body.getElementsByTagName("*");
+
+for (var i = 0; i < e.length; i++) {
+  e[i].style.color = "mediumvioletred";
+  e[i].style.borderColor = "blueviolet";
+  e[i].style.borderBlockColor = "mediumspringgreen";
+  // e[i].style.outlineColor = "green";
+  // e[i].style.scrollbarColor = "green";
+  e[i].style.linkColor = "green";
+  e[i].style.background = "rgba(0, 0, 0, 0.1)";
+}
+
+// document.body.style.scrollbarColor = "green";
+// document.body.style.linkColor = "green";
+
+
+document.body.style.backgroundImage = "url('https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/363319939_2298553893668719_8184542833059679631_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_ohc=uQOqqKTXNh8AX90eQsF&_nc_ht=scontent-lga3-2.xx&oh=00_AfC_FoTScqgVg-mCmlBZmh7-ExYPIw6v8uUpls4GgK6JPA&oe=64CE1BC9')";
+// object.style.backgroundSize = "auto|length|cover|contain|intial|inherit"
+document.body.style.backgroundSize = "cover"; // Adjust the background size as needed
+
+
+document.body.addEventListener('mouseover', function(event) {
+    event.target.style.color = "deeppink";
+});
+
+document.body.addEventListener('mouseout', function(event) {
+    event.target.style.color = "mediumvioletred";
+});
